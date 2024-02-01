1e58148100a038f9577862cbaa78c0a44c4255cb
tadeotech <tadeo.tech@proton.me>
2024-02-01 09:11:14 -0600

    initialize firebase and firestore explicitly

--- terraform/modules/firebase/main.tf
+++ terraform/modules/firebase/main.tf
@@ -16,17 +16,18 @@
  */
 
 # Enabling a firbase project
-
-resource "google_app_engine_application" "firebase_init" {
-  # Only execute this module when global var firebase_init set as "true"
-  # NOTE: the Firebase can only be initialized once (via App Engine).
-  count = var.firebase_init ? 1 : 0
-
-  provider      = google-beta
-  project       = var.project_id
-  location_id   = var.firestore_region
-  database_type = "CLOUD_FIRESTORE"
+resource "google_firebase_project" "default-firebase" {
+  project = var.project_id
 }
+
+# Creating the firestore instance
+resource "google_firestore_database" "default-firestore" {
+  project       = var.project_id
+  type          = "FIRESTORE_NATIVE"
+  name          = "(default)"
+  location_id   = var.firestore_region
+}
+
 
 resource "google_storage_bucket" "firestore-backup-bucket" {
   name          = "${var.project_id}-firestore-backup"
